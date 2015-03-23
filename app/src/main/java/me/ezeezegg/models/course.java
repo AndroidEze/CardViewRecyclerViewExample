f13941f8bf8caa1b21cca76ca4140b43822ae845
Ezequiel García <ezeezegg@gmail.com>
2015-03-22 22:00:10 -0600

    generate model course

---
+++ app/src/main/java/me/ezeezegg/models/course.java
@@ -0,0 +1,35 @@
+package me.ezeezegg.models;
+
+/**
+ * Created by egarcia on 3/22/15.
+ */
+public class course {
+
+    private Integer id;
+    private String name;
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
